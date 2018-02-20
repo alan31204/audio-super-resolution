7190197ebb2ad48f56ae18e5c101fda8ac70e83c
Alan <pchiu4@u.rochester.edu>
2018-02-20 00:22:38 -0500

    try

--- src/audionet.py
+++ src/audionet.py
@@ -1,3 +1,12 @@
 import numpy as np
 import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.autograd import lr_scheduler
 from torch.autograd import Variable
+import torchvision
+from torchvision import datasets, models, transforms
+import matplotlib.pyplot as plt
+import time
+import os
+# import copy
