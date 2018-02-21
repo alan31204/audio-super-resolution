6cae8a7036ac7e69ff399a6ec654c3c5b0d3c3c9
PO CHUN CHIU <pchiu4@bh25fen.circ.rochester.edu>
2018-02-20 20:59:33 -0500

    loader init

--- data/vctk/loader.py
+++ data/vctk/loader.py
@@ -1,26 +1,15 @@
 import h5py
+import numpy
 import torch.utils.data.Dataset
 
-# single speaker training dataset 
-filename = 'vctk-speaker1-train.4.16000.8192.4096.h5'
-# single speaker validation dataset
-#filename = 'vctk-speaker1-val.4.16000.8192.4096.h5'
-
-f = h5py.File(filename, 'r')
-
-# List all groups
-print("Keys: %s" % f.keys())
-a_group_key = list(f.keys())[0]
-b_group_key = list(f.keys())[1]
-
-# Get the data
-data = list(f[a_group_key])
-label = list(f[b_group_key])
+# data loading from PyTorch tutorial
 
 class loading(Dataset):
+    # filename = 'vctk-speaker1-train.4.16000.8192.4096.h5'
     root_dir = 'vctk-speaker1-train.4.16000.8192.4096.h5'
 
-    def __init__(self, csv_file, root_dir, transform=None):
+
+    def __init__(self, root_dir, transform=None):
        """
 	vctk-speaker1-train.4.16000.8192.4096.h5
 	vctk-speaker1-val.4.16000.8192.4096.h5
@@ -30,22 +19,83 @@ class loading(Dataset):
             transform (callable, optional): Optional transform to be applied
                 on a sample.
         """
-        self.landmarks_frame = pd.read_csv(csv_file)
+        f = h5py.File(root_dir, 'r')
+
+	# List all groups
+        print("Keys: %s" % f.keys())
+        a_group_key = list(f.keys())[0]
+        b_group_key = list(f.keys())[1]
+
+        # Get the data
+        data = list(f[a_group_key])
+        label = list(f[b_group_key])
+
+	# self.landmarks_frame = pd.read_csv(csv_file)
         self.root_dir = root_dir
+	self.data = data
+	self.label = label
         self.transform = transform
 
     def __len__(self):
-        return len(self.landmarks_frame)
+        return len(self.data)
 
     def __getitem__(self, idx):
-        img_name = os.path.join(self.root_dir,
-                                self.landmarks_frame.iloc[idx, 0])
-        image = io.imread(img_name)
-        landmarks = self.landmarks_frame.iloc[idx, 1:].as_matrix()
-        landmarks = landmarks.astype('float').reshape(-1, 2)
-        sample = {'image': image, 'landmarks': landmarks}
+        #img_name = os.path.join(self.root_dir,
+         #                       self.landmarks_frame.iloc[idx, 0])
+        #image = io.imread(img_name)
+        #landmarks = self.landmarks_frame.iloc[idx, 1:].as_matrix()
+        #landmarks = landmarks.astype('float').reshape(-1, 2)
+        sample = {'lr': self.data[idx], 'hr': self.label[idx]}
 
-        "if self.transform:
-            sample = self.transform(sample)"
+        if self.transform:
+            sample = self.transform(sample)
 
         return sample
+
+
+transformed_dataset = loading(transform=transforms.Compose([Rescale(256),RandomCrop(224),ToTensor()]))
+
+for i in range(len(transformed_dataset)):
+    sample = transformed_dataset[i]
+
+    print(i, sample['lr'].size(), sample['hr'].size())
+
+    if i == 3:
+        break
+
+dataloader = DataLoader(transformed_dataset, batch_size=4,
+                        shuffle=True, num_workers=4)
+
+
+# Helper function to show a batch
+def show_landmarks_batch(sample_batched):
+    """Show image with landmarks for a batch of samples."""
+    images_batch, landmarks_batch = \
+            sample_batched['lr'], sample_batched['hr']
+    batch_size = len(images_batch)
+    im_size = images_batch.size(2)
+
+    grid = utils.make_grid(images_batch)
+    plt.imshow(grid.numpy().transpose((1, 2, 0)))
+
+    for i in range(batch_size):
+        plt.scatter(landmarks_batch[i, :, 0].numpy() + i * im_size,
+                    landmarks_batch[i, :, 1].numpy(),
+                    s=10, marker='.', c='r')
+
+        plt.title('Batch from dataloader')
+
+for i_batch, sample_batched in enumerate(dataloader):
+    print(i_batch, sample_batched['lr'].size(),
+          sample_batched['hr'].size())
+
+    # observe 4th batch and stop.
+    if i_batch == 3:
+        plt.figure()
+        show_landmarks_batch(sample_batched)
+        plt.axis('off')
+        plt.ioff()
+        plt.show()
+        break
+
+
