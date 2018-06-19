bd0b22bffebccca4c3358f98ef4cbe8cc12c053e
Alan <pchiu4@u.rochester.edu>
2018-06-19 14:57:56 -0400

    summer project starts

--- src/models/audionet.py
+++ src/models/audionet.py
@@ -11,12 +11,19 @@ import time
 import os
 # import copy
 
-class AudioNet(torch.nn.Module):
+
+# TODO: set up the audio net model
+# learn PyTorch
+# read Wavenet paper, and understand its structures
+
+
+class AudioNet(nn.Module):
 # Generic PyTorch model training code
-	# def __init__(self, from_ckpt=False, n_dim=None, r=2, opt_params=default_opt, log_prefix='./run'):
-	def __init__(self, block, layers):
-		super(AudioNet, self).__init__()
-		self.in_channels = 16
+	def __init__(self, from_ckpt=False, n_dim=None, r=2, opt_params=default_opt):
+
+	# def __init__(self, block, layers):
+	# 	super(AudioNet, self).__init__()
+	# 	self.in_channels = 16
 		
 
     # perform the usual initialization
@@ -24,7 +31,9 @@ class AudioNet(torch.nn.Module):
     Model.__init__(self, from_ckpt=from_ckpt, n_dim=n_dim, r=r,
                    opt_params=opt_params, log_prefix=log_prefix)
 
-    def reate_model(self, n_dim, r):
+
+
+    def create_model(self, n_dim, r):
 
 
 
