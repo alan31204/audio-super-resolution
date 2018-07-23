a795aed18d8fa60d0d6c8f09bf2d512bd33cf3c9
Alan <pchiu4@u.rochester.edu>
2018-07-23 15:37:46 -0400

    first implementation of the network structure

--- src/models/audionet.py
+++ src/models/audionet.py
@@ -17,8 +17,8 @@ import math
 # Pytorch Official ResNet sample model
 # read Wavenet paper, and understand its structures
 
-__all__ = ['ResNet', 'resnet18', 'resnet34', 'resnet50', 'resnet101',
-           'resnet152']
+# __all__ = ['ResNet', 'resnet18', 'resnet34', 'resnet50', 'resnet101',
+#            'resnet152']
 
 model_urls = {
     'resnet18': 'https://download.pytorch.org/models/resnet18-5c106cde.pth',
@@ -33,91 +33,93 @@ def conv3x3(in_planes, out_planes, stride=1):
     """3x3 convolution with padding"""
     return nn.Conv1d(in_planes, out_planes, kernel_size=3, stride=stride,
                      padding=0, bias=False)
-    # return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
-    #                  padding=1, bias=False)
 
 
 class AudioNet(nn.Module):
 # Generic PyTorch model training code
     # def __init__(self, from_ckpt=False, n_dim=None, r=2, opt_params=default_opt):
 
-    def __init__(self, block, layers, num_classes=1000):
-        self.inplanes = 64
+    def __init__(self, layers, num_classes=1000):
+        # self.inplanes = 64
         super(AudioNet, self).__init__()
-        self.dconv1 = DLayer(128, 128, 65, stride=1, 32)
-        self.dconv2 = DLayer(256, 256, 33, stride=1, 112)
-        self.dconv3 = DLayer(512, 512, 17, stride=1, 248)
-        self.dconv4 = DLayer(512, 512, 9, stride=1, 252)
-        self.dconv5 = DLayer(512, 512, 9, stride=1, 252)
-        self.dconv6 = DLayer(512, 512, 9, stride=1, 252)
-        self.dconv7 = DLayer(512, 512, 9, stride=1, 252)
-        self.dconv8 = DLayer(512, 512, 9, stride=1, 252)
-        self.bneck1 = Bottleneck(512, 512, stride=1)
-        self.uconv1 = ULayer()
-        self.uconv2 = ULayer()
-        self.uconv3 = ULayer()
-        self.uconv4 = ULayer()
-        self.uconv5 = ULayer()
-        self.uconv6 = ULayer()
-        self.uconv7 = ULayer()
-        self.uconv8 = ULayer()
+        self.dconv1 = DLayer(128, 128, 65, stride=2, 32)
+        self.dconv2 = DLayer(256, 256, 33, stride=2, 16)
+        self.dconv3 = DLayer(512, 512, 17, stride=2, 8)
+        self.dconv4 = DLayer(512, 512, 9, stride=2, 4)
+        self.dconv5 = DLayer(512, 512, 9, stride=2, 4)
+        self.dconv6 = DLayer(512, 512, 9, stride=2, 4)
+        self.dconv7 = DLayer(512, 512, 9, stride=2, 4)
+        self.dconv8 = DLayer(512, 512, 9, stride=2, 4)
+        self.bneck1 = Bottleneck(512, 512, stride=2)
+        self.uconv1 = ULayer(512, 512, 9, stride=1, 4)
+        self.uconv2 = ULayer(512, 512, 9, stride=1, 4)
+        self.uconv3 = ULayer(512, 512, 9, stride=1, 4)
+        self.uconv4 = ULayer(512, 512, 9, stride=1, 4)
+        self.uconv5 = ULayer(512, 512, 9, stride=1, 4)
+        self.uconv6 = ULayer(512, 512, 17, stride=1, 8)
+        self.uconv7 = ULayer(256, 256, 33, stride=1, 16)
+        self.uconv8 = ULayer(128, 128, 65, stride=1, 32)
         self.fconv  = FinalConv(2, 2, stride=1)
 
         # self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3,
         #                        bias=False)
-        self.bn1 = nn.BatchNorm2d(64)
+        self.bn1 = nn.BatchNorm1d(64)
         self.relu = nn.ReLU(inplace=True)
-        # maxpool ?
-    #   self.in_channels = 16
-        
-
-    # perform the usual initialization
-    # self.r = r
-    # Model.__init__(self, from_ckpt=from_ckpt, n_dim=n_dim, r=r,
-    #                opt_params=opt_params, log_prefix=log_prefix)
 
 
+        for m in self.modules():
+            if isinstance(m, nn.Conv1d):
+                # nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+                nn.init.orthogonal(m.weight) # orthogonal initialization
+            elif isinstance(m, nn.BatchNorm1d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
 
-    def _make_layer(self, block, planes, blocks, stride=1):
-        downsample = None
-        if stride != 1 or self.inplanes != planes * block.expansion:
-            downsample = nn.Sequential(
-                nn.Conv1d(self.inplanes, planes * block.expansion,
-                          kernel_size=1, stride=stride, bias=False),
-                nn.BatchNorm2d(planes * block.expansion),
-            )
+    # def _make_layer(self, block, planes, blocks, stride=1):
+    #     downsample = None
+    #     if stride != 1 or self.inplanes != planes * block.expansion:
+    #         downsample = nn.Sequential(
+    #             nn.Conv1d(self.inplanes, planes * block.expansion,
+    #                       kernel_size=1, stride=stride, bias=False),
+    #             nn.BatchNorm2d(planes * block.expansion),
+    #         )
 
-        layers = []
-        layers.append(block(self.inplanes, planes, stride, downsample))
-        self.inplanes = planes * block.expansion
-        for i in range(1, blocks):
-            layers.append(block(self.inplanes, planes))
+    #     layers = []
+    #     layers.append(block(self.inplanes, planes, stride, downsample))
+    #     self.inplanes = planes * block.expansion
+    #     for i in range(1, blocks):
+    #         layers.append(block(self.inplanes, planes))
 
-        return nn.Sequential(*layers)
+    #     return nn.Sequential(*layers)
 
 
     def forward(self, x):
-        # x = self.conv1(x)
-        # x = self.bn1(x)
-        # x = self.relu(x)
         # Forward Downsample pass
-        x = self.dconv1(x)
-        x = self.dconv2(x)
-        x = self.dconv3(x)
-        x = self.dconv4(x)
-        x = self.dconv5(x)
-        x = self.dconv6(x)
-        x = self.dconv7(x)
-        x = self.dconv8(x)
+        x1 = self.dconv1(x)
+        x2 = self.dconv2(x1)
+        x3 = self.dconv3(x2)
+        x4 = self.dconv4(x3)
+        x5 = self.dconv5(x4)
+        x6 = self.dconv6(x5)
+        x7 = self.dconv7(x6)
+        x8 = self.dconv8(x7)
 
-        x = self.bneck1(x)
-        
+        b = self.bneck1(x8)
 
-        # x = self.avgpool(x)
-        x = x.view(x.size(0), -1)
-        x = self.fconv(x)
+        # Forward Upsample pass
+        h1 = self.uconv1(x8)
+        h2 = self.uconv1(x7)
+        h3 = self.uconv1(x6)
+        h4 = self.uconv1(x5)
+        h5 = self.uconv1(x4)
+        h6 = self.uconv1(x3)
+        h7 = self.uconv1(x2)
+        h8 = self.uconv1(x1)
 
-        return x
+
+        out = self.fconv(h8)
+
+        return out
 
 
 
@@ -125,12 +127,12 @@ class AudioNet(nn.Module):
 class DLayer(nn.Module):
     expansion = 1
 
-    def __init__(self, inplanes, planes, kernel_size, stride=1, padding, downsample=None):
+    def __init__(self, inplanes, planes, kernel_size, stride=2, padding, downsample=None):
         super(DLayer, self).__init__()
         # self.conv1 = conv3x3(inplanes, planes, stride)
         self.conv1 = nn.Conv1d(in_planes, out_planes, kernel_size=kernel_size, stride=stride,
                      padding=padding, bias=False) # padding not sure
-        self.bn1 = nn.BatchNorm2d(planes)
+        # self.bn1 = nn.BatchNorm2d(planes)
         self.relu = nn.LeakyReLU(0.2)
         self.downsample = downsample
         self.stride = stride
@@ -191,7 +193,7 @@ class ULayer(nn.Module):
 class Bottleneck(nn.Module):
     expansion = 4
 
-    def __init__(self, inplanes, planes, stride=1, downsample=None):
+    def __init__(self, inplanes, planes, stride=2, downsample=None):
         super(Bottleneck, self).__init__()
         self.conv1 = nn.Conv1d(inplanes, planes, kernel_size=9, bias=False)
         self.bn1 = nn.BatchNorm2d(planes)
--- src/train.py
+++ src/train.py
@@ -1,31 +1,108 @@
-import os
+import os, sys
 os.sys.path.append(os.path.abspath('.'))
 os.sys.path.append(os.path.dirname(os.path.abspath('.')))
 
 import argparse
 import numpy as np
 import models
-from models.model import default_opt
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from torchvision import datasets, transforms
+# from models.model import default_opt
+from models import AudioNet
 from models.io import load_h5, upsample_wav
 
 # TODO list for training the model
+# From Kuleshov's run.py
 
 # processing 
 def make_parser():
+	parser = argparse.ArgumentParser(description='ASR')
+  	subparsers = parser.add_subparsers(title='Commands')
+
+  	# train
+
+	train_parser = subparsers.add_parser('train')
+	train_parser.set_defaults(func=train)
+
+	train_parser.add_argument('--train', required=True,
+	    help='path to h5 archive of training patches')
+	train_parser.add_argument('--val', required=True,
+	    help='path to h5 archive of validation set patches')
+	train_parser.add_argument('-e', '--epochs', type=int, default=100,
+	    help='number of epochs to train')
+	train_parser.add_argument('--batch-size', type=int, default=128,
+	    help='training batch size')
+	train_parser.add_argument('--logname', default='tmp-run',
+	    help='folder where logs will be stored')
+	train_parser.add_argument('--layers', default=4, type=int,
+	    help='number of layers in each of the D and U halves of the network')
+	train_parser.add_argument('--alg', default='adam',
+	    help='optimization algorithm')
+	train_parser.add_argument('--lr', default=1e-3, type=float,
+	    help='learning rate')
+	train_parser.add_argument('--momentum', default=0.9, type=float, metavar='M',
+                    help='momentum')
+
+	  # eval
+
+	eval_parser = subparsers.add_parser('eval')
+	eval_parser.set_defaults(func=eval)
+
+	eval_parser.add_argument('--logname', required=True,
+	    help='path to training checkpoint')
+	eval_parser.add_argument('--out-label', default='',
+	    help='append label to output samples')
+	eval_parser.add_argument('--wav-file-list', 
+	    help='list of audio files for evaluation')
+	eval_parser.add_argument('--r', help='upscaling factor', type=int)
+	eval_parser.add_argument('--sr', help='high-res sampling rate', 
+	                                   type=int, default=16000)
+	
+	return parser
 
 
 # training process 
 def train(args):
+	# get data
+  	X_train, Y_train = load_h5(args.train)
+ 	X_val, Y_val = load_h5(args.val)
+
+
+def eval(args):
+
+
 
 
 # make and create model for training and evaluating
-def get_model(args, n_dim, r, train=True):
+def get_model(args, num_classes, train=True):
+	"""Create a model based on arguments"""  
+  	if train:
+    	opt_params = { 'alg' : args.alg, 'lr' : args.lr, 'b1' : 0.9, 'b2' : 0.999,
+                   'batch_size': args.batch_size, 'layers': args.layers }
+  	# SGD optimizer
+  	# else: 
+   #  	opt_params = default_opt
+
+  	# create model
+
+  	# model = models.AudioNet(num_classes=num_classes, r=r, 
+    #                             opt_params=opt_params, log_prefix=args.logname)
+  	return model
+
+
+
 
 
 
 def main():
   parser = make_parser()
+
   args = parser.parse_args()
+  use_cuda = torch.cuda.is_available()
+  optimizer = optim.Adam(net_model.parameters(), lr=1e-3)
   args.func(args)
 
 
