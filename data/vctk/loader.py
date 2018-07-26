555e46aa422db7d6be787a59aa0abf68ee2a9f06
Alan <pchiu4@u.rochester.edu>
2018-07-26 19:33:47 -0400

    debug process

--- data/vctk/loader.py
+++ data/vctk/loader.py
@@ -24,8 +24,8 @@ class loading(Dataset):
             # print ('List of arrays in input file:', hf.keys())
             data = np.array(hf.get('data'))
             label = np.array(hf.get('label'))
-            print ('Shape of X:', data.shape)
-            print ('Shape of Y:', label.shape)
+            print (data.shape)
+            print (label.shape)
 
 	# self.landmarks_frame = pd.read_csv(csv_file)
         self.root_dir = root_dir
