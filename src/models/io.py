7346df28cb3e668d502ee7c8cf0b447b92204d77
Alan <pchiu4@u.rochester.edu>
2018-08-08 10:19:39 -0400

    try to eval

--- src/models/io.py
+++ src/models/io.py
@@ -39,6 +39,10 @@ def upsample_wav(wav, model):
 	# upscale the low-res version
 	# P = model.predict(x_lr.reshape((1,len(x_lr),1)))
 	# x_pr = P.flatten()
+	print("LR: ")
+	print(x_lr.size())
+	print("HR")
+	print(x_hr.size())
 	x_lr = torch.from_numpy(x_lr)
 	P = model(x_lr)
 	# x_pr = P.flatten()
