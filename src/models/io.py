125a06cddcdc91bd282b8b863a3f46d65ffce13a
Alan <pchiu4@u.rochester.edu>
2018-08-08 14:51:17 -0400

    try to evaluate

--- src/models/io.py
+++ src/models/io.py
@@ -64,9 +64,10 @@ def upsample_wav(wav, model):
 	outname = wav + '.' + out_label
 	rand = 4
 	print(fs)
-	print(x_lr.shape)
+	print(x_lr)
+	print(x_pr)
 	librosa.output.write_wav(outname + '.hr.wav', x_hr, fs)	
-	librosa.output.write_wav(outname + '.lr.wav', x_lr, fs / rand)	
+	librosa.output.write_wav(outname + '.lr.wav', x_lr[0], fs / rand)	
 	librosa.output.write_wav(outname + '.pr.wav', x_pr, fs)	
 
 	# save the spectrum
